498743ac784bc80c251924482bc9d39e3def0b91
Selim Altayev <selim.altayev@gmail.com>
2025-08-29 19:57:59 +0200

    Initial project commit

---
+++ storage/framework/views/f2513f422cfce4af90d6d184a34ace6d.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>RG-OilGas.com</title>
+    </head>
+
+    <body>
+        RG-OilGas.com
+    </body>
+
+</html>
+<?php /**PATH /var/www/rg-oilgas.com/resources/views/welcome.blade.php ENDPATH**/ ?>
